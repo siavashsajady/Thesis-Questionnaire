6a1bce9f9cb876779efd726ee2b62700d78c5535
Siavash <siavash.sajjady@gmail.com>
2019-03-03 17:20:21 +0000

    show answer the Qestion on the answer page

--- app.js
+++ app.js
@@ -1,26 +1,51 @@
 var express = require("express");
 var app = express();
+var mongoose = require("mongoose");
 var bodyParser = require("body-parser");
 
+mongoose.connect('mongodb://localhost:27017/sami-pro-v5', { useNewUrlParser: true });
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended : true}));
 app.set("view engine", "ejs");
 
+
+//SCHEMA SETUP
+
+var projectSchema = new mongoose.Schema({
+    ques : String,
+    answ : String
+});
+
+var Project = mongoose.model("Project", projectSchema);
+
+// Project.create({
+//      ques :" What would be the requirements for the use of internal and external TI?",
+//     answ : "lab lab lab laaab labb laaababa and so it "
+// }, function(err,myque){
+//     if(err){
+//         console.log("Error!!!");
+//         console.log(err);
+//     } else {
+//         console.log("This is Resul of Question: ");
+//         console.log(myque);
+//     }
+// });
+
+
+
 // ROUTE
 
- var questions = [
-      {ques : "what is your name?", answ : "siavash"},
-      {ques : "where do yuo leave?", answ : "stockholm"}
-      ]
-
+// INDEX - show home page
 app.get("/", function(req,res){
     res.render("home");
 });
 
+// About - show about page
 app.get("/about",function(req, res) {
     res.render("about");
 });
 
+// 
 app.get("/signup", function(req,res){
    res.render("signup"); 
 });
@@ -31,8 +56,8 @@ app.get("/consentform", function(req,res){
 });
 
 app.get("/question", function(req,res){
- 
-   res.render("question", {anyname : questions}); 
+    
+             res.render("question");
 });
 
 app.post("/question", function(req,res){
@@ -40,14 +65,29 @@ app.post("/question", function(req,res){
    var answ = req.body.answ; 
    var newAnswer = {ques: ques, answ : answ}
    
-   questions.push(newAnswer);
-   //redirect
-   res.redirect("/question");
+   //create a new qesform and save in DB
+   Project.create(newAnswer, function(err, newlyCreated){
+       if(err){
+           console.log(err);
+       } else {
+           res.redirect("/question");
+       }
+   });
 });
 
-// app.get("/answer", function(req,res){
-//   res.render("answer"); 
-// });
+
+
+
+app.get("/answer", function(req,res){
+   // get all ques and answ from DB
+    Project.find({}, function(err,allanswers){
+        if(err){
+            console.log(err);
+        } else {
+             res.render("answer", {anyname : allanswers});
+        }
+    });
+});
 
 
 app.listen(process.env.PORT, process.env.IP,function(){
